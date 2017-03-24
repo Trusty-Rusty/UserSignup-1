ab3f70acf0531d75461f42d7dbde35e9872b40f1
Chris Bay <chris@chrisbay.net>
2017-03-24 13:24:59 -0500

    User Signup solution

---
+++ src/UserSignup/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if (user.Password == null || !user.Password.Equals(verify))
+            {
+                ViewBag.username = user.Username;
+                ViewBag.email = user.Email;
+                ViewBag.message = "Passwords do not match";
+
+                return View();
+            }
+
+            ViewBag.username = user.Username;
+
+            return View("Index");
+        }
+    }
+}
